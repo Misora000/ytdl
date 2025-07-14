a7c979b1edf368121be1f16d9bc8b976e8af7f2c
Misora <misora000@gmail.com>
2025-07-15 03:39:21 +0800

    impl some features and update README

--- README.md
+++ README.md
@@ -1,20 +1,53 @@
-ytdl a simple youtube-dl caller
-- Auto select the high quality video (up to 4k 2160p 60fps)
-- Auto retry for network error
+# ytdl: A lazy youtube-dl launcher
 
-## Setup and usage:
-Get the latest binary.
-- ffmpeg: https://www.ffmpeg.org/
-- yt-dlp: https://github.com/yt-dlp/yt-dlp/releases
+Feature
+- Auto select the highest resolution (up to 4k 2160p 60fps)
+- Auto retry for network error and customize retry num by attribe `-c`
+- Specify saving path by attribute `-o`
+- Auto rename when collision by attribute `-s`
+- Auto use cookie from `www.youtube.com_cookies.txt` (by [Get cookies.txt LOCALLY](https://chromewebstore.google.com/detail/get-cookiestxt-locally/cclelndahbckbenkjhflpdbgdldlbecc))
+- Optional to save video as generic name `youtube video #XXXXXXXXXXX`.mkv by attribe `-g`
+- Batch mode & file mode (similar to `yt-dlp -a`)
+- Summarize list of download failed & qulity downgraded videos
 
-Setup the following dependency and make link/alias/path in functional.
-- Pyhton 3
-- ffmpeg: ffmpeg.exe, ffplay.exe, ffprobe.exe
-- bin/yt-dlp.exe
+## Requirement
+Make sure you have the following executable in your `PATH`
+- [ffmpeg](https://www.ffmpeg.org/)
+- [yt-dlp](https://github.com/yt-dlp/yt-dlp/releases)
 
 
-
-Run in terminal:
+## Usage
 ```sh
-python main.py {youtube_video_url}
+usage: main.py [-h] [-c C] [-o O] [-f] [-s] [-g] urls
+
+positional arguments:
+  urls        Target url(s)
+
+options:
+  -h, --help  show this help message and exit
+  -c C        Retry count (default=1)
+  -o O        Output path
+  -f          File containing URLs to download
+  -s          Rename video if exists the same name
+  -g          Save video as the generic name
 ```
+
+Example
+```sh
+# download an url
+python main.py 'https://www.youtube.com/watch?v=0123456789A'
+
+# download multiple urls (separate by comma)
+python main.py 'https://www.youtube.com/watch?v=0123456789A,https://www.youtube.com/watch?v=0123456789B'
+
+# download multiple urls from file (one line one url)
+python main.py -f 'url_list.txt'
+
+# auto rename video when collision
+python main.py -s 'https://www.youtube.com/watch?v=0123456789A'
+
+# download to a specific folder
+python main.py -o 'download' 'https://www.youtube.com/watch?v=0123456789A'
+```
+
+You can pre-define default download folder in `default_dl_path.txt`
--- main.py
+++ main.py
@@ -1,59 +1,151 @@
 import os, sys
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('urls', help='Target url(s)')
+parser.add_argument('-c', help='Retry count (default=1)', type=int, default=1)
+parser.add_argument('-o', help='Output path', type=str, default='')
+parser.add_argument('-f', help='File containing URLs to download', default=False, action='store_true')
+parser.add_argument('-s', help='Rename video if exists the same name', default=False, action='store_true')
+parser.add_argument('-g', help='Save video as the generic name', default=False, action='store_true')
 
 bin_youtubedl = 'yt-dlp.exe'
-dst = 'E:/動畫/YoutubeDL'
 
 class ytdl():
     url = ''
 
-    def __init__(self, url):
+    def __init__(self, url, dst):
         self.url = url
+        self.dst = dst
+        self.append_id = False
+        self.generic_name = False
 
-    def get_format(self):
+        self.cookie = ''
+        if os.path.exists('www.youtube.com_cookies.txt'):
+             self.cookie = ' --cookies www.youtube.com_cookies.txt'
+
+    def append_id_to_video_name(self, val: bool):
+        self.append_id = val
+
+    def save_generic_name(self, val: bool):
+        self.generic_name = val
+
+    def get_video_title(self) -> str:
+        cmd = f'{bin_youtubedl}{self.cookie} --get-title {self.url}'
+        result = subprocess.run(cmd, capture_output=True, text=True, shell=True)
+        return result.stdout.replace('\n', '')
+        
+    def get_formats(self) -> list[str]:
         allFmt = []
-        cmd = [bin_youtubedl, '-F', self.url]
+        cmd = f'{bin_youtubedl}{self.cookie} -F {self.url}'
         result = subprocess.run(cmd, stdout=subprocess.PIPE)
 
-        for l in result.stdout.decode('ascii').split('\n'):
+        # for l in result.stdout.decode('ascii').split('\n'):
+        for l in result.stdout.decode('utf-8').split('\n'):
             if l.find('mp4') > 0:
                 allFmt.append(l.split(' ')[0])
             if l.find('webm') > 0:
                 allFmt.append(l.split(' ')[0])
 
-        # prefer video format:
-        #   315: 2160p, 60fps
-        #   313: 2160p, 30fps
-        #   308: 1440p, 60fps
-        #   271: 1440p, 30fps
-        #   299: 1080p, 60fps
-        #   137: 1080p, 30fps
-        for v in ['315', '313', '308', '271', '299', '137']:
+        preferFmts = [
+            '628', '315', # 2160p, 60fps
+            '625', '313', # 2160p, 30fps
+            '623', '308', # 1440p, 60fps
+            '620', '271', # 1440p, 30fps
+            '617', '299', # 1080p, 60fps
+            '614', '137', # 1080p, 30fps
+        ]
+
+        avaliableFmts = []
+        for v in preferFmts:
             if v in allFmt:
-                return v
+                avaliableFmts.append(v)
         
-        return ''
+        return avaliableFmts
 
     def download(self, vfmt):
+        attr_vfmt = f' -f {vfmt}+bestaudio --merge-output-format mkv'
         if vfmt == '':
-            cmd = '{} {} -o {}/%(title)s.mkv'.format(bin_youtubedl, self.url, dst)
-        else:
-            cmd = '{} -f {}+bestaudio --merge-output-format mkv {} -o {}/%(title)s.mkv'.format(bin_youtubedl, vfmt, self.url, dst)
-        return os.system(cmd)
+            attr_vfmt = ''
 
+        if self.generic_name:
+            cmd = f'{bin_youtubedl}{self.cookie}{attr_vfmt} {self.url} -o "{self.dst}/youtube video #%(id)s.mkv"'
+        elif self.append_id:
+            cmd = f'{bin_youtubedl}{self.cookie}{attr_vfmt} {self.url} -o "{self.dst}/%(title)s [%(id)s].mkv"'
+        else:
+            cmd = f'{bin_youtubedl}{self.cookie}{attr_vfmt} {self.url} -o "{self.dst}/%(title)s.mkv"'
+        return os.system(cmd)
+    
+    def autoRetryDownload(self, vfmt, count) -> bool:
+        for i in range(count):
+            if self.download(vfmt) == 0:
+                return True
+        return False
+
+
+def is_url(text) -> bool:
+    return len(text) > 0
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('insufficient parameters')
-        exit
+    args = parser.parse_args()
 
-    urls = sys.argv[1]
-    for url in urls.split(','):
-        d = ytdl(url)
-        bestVideo = d.get_format()
-        print('select vidoe format: {}'.format(bestVideo))
-        
-        while True:
-            status = d.download(bestVideo)
-            if status == 0:
+    urls = []
+    if args.f:
+        with open(args.urls, 'r') as f:
+            urls = f.read().split('\n')
+    else:
+        urls = args.urls.split(',')
+
+    # parse downlaod destination
+    if len(args.o) == 0:
+        if os.path.exists('default_dl_path.txt'):
+            with open('default_dl_path.txt') as fr:
+                args.o = fr.readline().replace('\n', '')
+        else:
+            args.o = 'dl'
+    
+    if not os.path.exists(args.o):
+        os.mkdir(args.o)
+
+
+    dlFailed = []
+    downgrade = []
+
+    for url in urls:
+        if not is_url(url):
+            continue
+
+        dlFailed.append(url)
+
+        d = ytdl(url, args.o)
+        vfmts = d.get_formats()
+
+        if args.g:
+            d.save_generic_name(True)
+
+        # check existence
+        if args.s:
+            title = d.get_video_title()
+            if os.path.exists(os.path.join(args.o, title+'.mkv')):
+                d.append_id_to_video_name(True)
+
+        for i in range(len(vfmts)):
+            print(f'select vidoe format: {vfmts[i]}')
+
+            if d.autoRetryDownload(vfmts[i], args.c+1):
+                # download successfull
+                dlFailed.pop()
+                if i > 0:
+                    downgrade.append(f'{url}\t{vfmts[0]} -> {vfmts[i]}')
                 break
+
+    if len(downgrade) > 0:
+        print('\n---- downgrade videos ----')
+        for v in downgrade:
+            print(v)
+
+    if len(dlFailed) > 0:
+        print('\n---- download failed ----')
+        for v in dlFailed:
+            print(v)
